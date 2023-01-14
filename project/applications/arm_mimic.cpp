39cc00aeefe00101b821c3ba73334dbcc9d2ffba
PhazonicRidley <ma13hew@gmail.com>
2023-01-13 19:21:00 -0800

    Finished converting code over

--- project/applications/arm_mimic.cpp
+++ project/applications/arm_mimic.cpp
@@ -1,16 +1,32 @@
 #include <libhal-util/steady_clock.hpp>
+#include <string>
 
 #include "../hardware_map.hpp"
-#include "headers/adc_mux_cd74hc4067.hpp"
+#include "implementations/adc_mux_cd74hc4067.hpp"
+#include "implementations/common.hpp"
+
+
 
 hal::status application(arm_mimic::hardware_map& p_map) {
     using namespace std::chrono_literals;
     using namespace hal::literals;
+    const uint8_t N = 6;
 
     while (true) {
+
         float adc_value = HAL_CHECK(p_map.adc_pin->read());
         HAL_CHECK(hal::delay(*p_map.steady_clock, 1000ms));
-        auto mux = AdcMuxCd74hc4067(p_map.adc_pin, p_map.signal_0, p_map.signal_1, p_map.signal_2, p_map.signal_3, p_map.steady_clock);
+        auto digital_multiplexer = AdcMuxCd74hc4067(p_map.adc_pin, p_map.signal_0, p_map.signal_1, p_map.signal_2, p_map.signal_3, p_map.steady_clock);
+        
+        std::array<uint8_t, N> channels = {0, 1, 2, 3, 4, 5};
+        std::array<float, N> degree_conversion = {360, 90, 90, 90, 360, 360}; 
+        auto output_voltages = HAL_CHECK(digital_multiplexer.read_all<N>(channels));
+        std::array<float, N> results = {};
+        for (auto i = 0; i < N; i++) {
+            float true_degree = arm_mimic::common::voltage_to_degree(output_voltages[i], 3.3, 360);
+            results[i] = HAL_CHECK(arm_mimic::common::degree_phase_shift(true_degree, degree_conversion[i]));
+        }
+        HAL_CHECK(arm_mimic::common::send_data_mc<N>(results));
     }
 
     return hal::success();
---
+++ project/applications/implementations/common.hpp
@@ -0,0 +1,52 @@
+#pragma once
+#include <string>
+#include <libhal/error.hpp>
+#include <assert.h> // TODO: remove after implemented
+#include <libhal-util/map.hpp>
+
+namespace arm_mimic::common {
+    template<uint8_t N>
+    hal::status send_data_mc(std::array<float, N> raw_data) {
+        // Structure of json file
+        /* 
+        { 
+            HB: 0,
+            IO: 1,
+            M: 'M',
+            Angles: [$ROTUNDA, $SHOULDER, $ELBOW, $WRIST_PITCH, $WRIST_ROLL, $END_EFFECTOR]
+        }
+        */
+    std::string json_str = "{\"HB\":\"0\",\"IO\":\"1\",\"M\":\"M\",\"CMD\":[";
+    for (auto i = 0; i < N; i++) {
+            if (i != N - 1)
+                json_str.append(std::to_string(raw_data[i]) + ",");
+            else
+                json_str.append(std::to_string(raw_data[i]));
+    }
+
+    json_str.append("]}");
+    // TODO POST to esp/output serial.
+    assert("POST to esp/output serial not implienmented");
+    return hal::success();
+    }
+
+    hal::result<float> degree_phase_shift(float input_degree, float new_origin) {
+        if (new_origin < 0 || new_origin > 360)
+            return hal::new_error("This function only considers the unit circle. Please set new_zero to the position on the unit circle it will be.");
+        
+        while (input_degree > 360 || input_degree < -360) {
+            // take modulus of a float without losing data, thanks Corey!
+            input_degree = input_degree / 360;
+            input_degree = (input_degree - static_cast<int>(input_degree)) * 360;
+        }
+
+        return input_degree - new_origin;
+    }
+
+    float voltage_to_degree(float value, float max_voltage, float max_degree) {
+        return hal::map(value, 
+        std::pair<float, float>(0, max_voltage), 
+        std::pair<float, float>(0, 360));
+    }
+
+} // arm_mimic::common
