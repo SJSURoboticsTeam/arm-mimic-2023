96b99acf62fcde2bb23117b2f5b07421c3548439
PhazonicRidley <ma13hew@gmail.com>
2023-02-16 21:42:15 -0800

    :art: Moved declarations outside of main loop

--- applications/app.cpp
+++ applications/app.cpp
@@ -20,16 +20,16 @@ using namespace std::chrono_literals;
 hal::status application(arm_mimic::hardware_map& p_map) {
 
   const uint8_t N = 6;
+  std::array<uint8_t, N> channels = {0, 1, 2, 3, 4, 5};
+  std::array<float, N> degree_conversion = {360, 180, 180, 180, 360, 180}; 
+  std::array<float, N> voltage_maxes = {0.3333, 3.3, 3.3, 3.3, 0.3333, 3.3};
+  std::array<float, N> true_degrees = {};
+  std::array<float, N> results = {};
+  auto digital_multiplexer = arm_mimic::AdcMuxCd74hc4067(p_map.adc_pin, p_map.signal_0, p_map.signal_1, p_map.signal_2, p_map.signal_3, p_map.steady_clock);
 
   while (true) {
-    auto digital_multiplexer = arm_mimic::AdcMuxCd74hc4067(p_map.adc_pin, p_map.signal_0, p_map.signal_1, p_map.signal_2, p_map.signal_3, p_map.steady_clock);
     
-    std::array<uint8_t, N> channels = {0, 1, 2, 3, 4, 5};
-    std::array<float, N> degree_conversion = {360, 180, 180, 180, 360, 180}; 
-    std::array<float, N> voltage_maxes = {0.3333, 3.3, 3.3, 3.3, 0.3333, 3.3};
     auto output_voltages = HAL_CHECK(digital_multiplexer.read_all<N>(channels));
-    std::array<float, N> true_degrees = {};
-    std::array<float, N> results = {};
     for (auto i = 0; i < N; i++) {
       float true_degree = arm_mimic::voltage_to_degree(output_voltages[i], voltage_maxes[i], 360);
       true_degrees[i] = true_degree;
--- applications/mimic_debug.cpp
+++ applications/mimic_debug.cpp
@@ -17,19 +17,20 @@
 using namespace hal::literals;
 using namespace std::chrono_literals;
 
+
+
 hal::status application(arm_mimic::hardware_map& p_map) {
 
   const uint8_t N = 6;
+  auto digital_multiplexer = arm_mimic::AdcMuxCd74hc4067(p_map.adc_pin, p_map.signal_0, p_map.signal_1, p_map.signal_2, p_map.signal_3, p_map.steady_clock);
+  std::array<uint8_t, N> channels = {0, 1, 2, 3, 4, 5};
+  std::array<float, N> degree_conversion = {360, 180, 180, 180, 360, 180}; 
+  std::array<float, N> voltage_maxes = {0.3333, 3.3, 3.3, 3.3, 0.3333, 3.3};
+  std::array<float, N> true_degrees = {};
+  std::array<float, N> results = {};
 
   while (true) {
-    auto digital_multiplexer = arm_mimic::AdcMuxCd74hc4067(p_map.adc_pin, p_map.signal_0, p_map.signal_1, p_map.signal_2, p_map.signal_3, p_map.steady_clock);
-    
-    std::array<uint8_t, N> channels = {0, 1, 2, 3, 4, 5};
-    std::array<float, N> degree_conversion = {360, 180, 180, 180, 360, 180}; 
-    std::array<float, N> voltage_maxes = {0.3333, 3.3, 3.3, 3.3, 0.3333, 3.3};
     auto output_voltages = HAL_CHECK(digital_multiplexer.read_all<N>(channels));
-    std::array<float, N> true_degrees = {};
-    std::array<float, N> results = {};
     for (auto i = 0; i < N; i++) {
         float true_degree = arm_mimic::voltage_to_degree(output_voltages[i], voltage_maxes[i], 360);
         true_degrees[i] = true_degree;
@@ -51,7 +52,7 @@ hal::status application(arm_mimic::hardware_map& p_map) {
     arm_mimic::print_array<float, N>(true_degrees, *p_map.terminal);
     HAL_CHECK(hal::delay(*p_map.steady_clock, 1ms));
     (void)hal::write(*p_map.terminal, "\n");
-    HAL_CHECK(hal::delay(*p_map.steady_clock, 250ms));
+    HAL_CHECK(hal::delay(*p_map.steady_clock, 500ms));
   }
 
   return hal::success();
